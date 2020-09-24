c90000bc55abf61a88075b2de1ba399f806875ca
youn yeo won <54175366+dudnjsckrgo@users.noreply.github.com>
2020-09-24 10:54:01 +0900

    test

--- crawler/controller.py
+++ crawler/controller.py
@@ -5,3 +5,13 @@ class Controller:
     def __init__(self):
         self.entity = Entity
         self.service = Service
+    def searching_data(self):
+        service = self.service
+
+        service.get_sichug_url()
+        service.get_corona_page()
+
+
+if __name__ == "__main__":
+    api = Controller()
+    api.searching_data()
